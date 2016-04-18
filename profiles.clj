35d7c3c6b6ebd7657b24d92a3f7ae0b58709ec19
Julien Chastang <julien.c.chastang@gmail.com>
2016-04-18 16:21:04 -0600

    refactor-nrepl "2.2.0"

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.11.0"]
-                  [refactor-nrepl "2.0.0"]
+                  [refactor-nrepl "2.2.0"]
                   [jonase/eastwood "0.2.3"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
